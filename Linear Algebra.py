3e28e603ef787009f8c4eb5b38d3c4f1a693ceaf
Dalen W Brauner <DalenWBrauner@gmail.com>
2013-07-10 14:57:00 -0400

    Finished Matrix object, started Sudoku object

--- Linear Algebra.py
+++ Linear Algebra.py
@@ -38,14 +38,19 @@ class Matrix(object):
                 self.row[x].append(values[index])
 
     def __str__(self):
-        superstring = ""
+        string = ""
         for row in self.row.values():
-            string = "(  "
+            string += "(  "
             for value in row:
                 string += str(value)+"  "
             string += ")\n"
-            superstring += string
-        return superstring
+        return string
+
+    def dimcol(self):
+        return self.nCols
+
+    def dimrow(self):
+        return self.nRows
 
     def getvalue(self,x,y):
         return self.M[str(x)+","+str(y)]
@@ -61,6 +66,30 @@ class Matrix(object):
         self.row[x][y] = value
         self.col[y][x] = value
 
+    def setrow(self,r,values):
+        # Checks this request will work in the first place
+        if   len(values) > self.nCols:  raise TypeError("Too many values; need "+\
+                                                        str(self.nCols)+".")
+        elif len(values) < self.nCols:  raise TypeError("Not enough values; need "+\
+                                                        str(self.nCols)+".")
+        else:
+            for n in xrange(self.nCols):
+                self.M[str(r)+","+str(n)] = values[n]
+                self.row[r][n] = values[n]
+                self.col[n][r] = values[n]
+
+    def setcol(self,c,values):
+        # Checks this request will work in the first place
+        if   len(values) > self.nRows:  raise TypeError("Too many values; need "+\
+                                                        str(self.nRows)+".")
+        elif len(values) < self.nRows:  raise TypeError("Not enough values; need "+\
+                                                        str(self.nRows)+".")
+        else:
+            for n in xrange(self.nRows):
+                self.M[str(n)+","+str(c)] = values[n]
+                self.row[n][c] = values[n]
+                self.col[c][n] = values[n]
+
     def swaprow(self,r1,r2):
         """Swaps row r1 with row r2."""
         # Changes the values of M appropriately
@@ -73,10 +102,10 @@ class Matrix(object):
             self.M[str(r1)+","+str(g)] = self.M[str(r2)+","+str(g)]
             self.M[str(r2)+","+str(g)] = temp    
         # Changes the values of the columns appropriately
-##        for col in self.col:
-##            temp = self.col[r1]
-##            self.col[r1] = self.col[r2]
-##            self.col[r2] = self.col[r1]
+        for col in self.col.values():
+            temp = col[r1]
+            col[r1] = col[r2]
+            col[r2] = temp
 
     def swapcol(self,c1,c2):
         """Swaps col c1 with col c2."""
@@ -90,8 +119,57 @@ class Matrix(object):
             self.M[str(g)+","+str(c1)] = self.M[str(g)+","+str(c2)]
             self.M[str(g)+","+str(c2)] = temp
         # Changes the values of the rows appropriately
-##        for row in self.row:
-##            temp = self.row[c1]
-##            self.row[c1] = self.row[c2]
-##            self.row[c2] = self.row[c1]
+        for row in self.row.values():
+            temp = row[c1]
+            row[c1] = row[c2]
+            row[c2] = temp
+
+class Sudoku(Matrix):
+    """A Sudoku Puzzle is a 9x9 Matrix with special functions for checking "sectors," or the
+    3x3 squares within the matricies."""
+    
+    # All a matrix needs is the number of rows and columns, and the values themselves.
+    def __init__(self, values):
+
+        self.nRows = 9
+        self.nCols = 9
+
+        # Checks if there are enough values.
+        if 81 != len(values):
+            raise TypeError("A Sudoku needs values.")
         
+        # The actual values are stored in a dictionary, with coordinates such as (0,0) & (1,2)
+        # being the keys.
+        self.M = {}
+        
+        # What sets apart a Matrix from a Dictionary is being able to access the rows/columns.
+        self.row = {}
+        self.col = {}
+
+        # What sets apart a Sudoku Puzzle from a Matrix are its 9 sectors.
+        self.sec = {}
+        
+        # The index is only used to keep track of which value needs to be recorded.
+        index = -1
+        
+        for x in xrange(9):
+            for y in xrange(9):
+                index += 1
+                
+                # For each value, the key is a string
+                Key = str(x)+","+str(y)
+                self.M[Key] = values[index]
+                
+                # If the column does not yet exist, create the list
+                if not self.col.has_key(y):     self.col[y] = []
+                self.col[y].append(values[index])
+                if not self.row.has_key(x):     self.row[x] = []
+                self.row[x].append(values[index])
+
+                # You're just going to have to trust me on this one.
+                if not self.sec.has_key(((x/3)*3)+(y/3)):
+                    self.sec(((x/3)*3)+(y/3)) = []
+                self.sec[((x/3)*3)+(y/3)].append(values[index])
+
+    def getsec(self,s):
+        return self.sec(s)
