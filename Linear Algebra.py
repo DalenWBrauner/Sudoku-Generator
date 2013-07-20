94c38e8c9dd912d3bbad8b6b9216f2442c3cf55d
Dalen W Brauner <DalenWBrauner@gmail.com>
2013-07-20 13:36:00 -0400

    Removed Matrix Object
    Sudoku object now encompasses all features, however still needs to be
    tweaked and finished to provide appropriate sector functionality.

--- Linear Algebra.py
+++ Linear Algebra.py
@@ -1,63 +1,90 @@
-class Matrix(object):
-    """A matrix is a set of numbers arranged in a grid with N rows and M columns."""
-
-    # All a matrix needs is the number of rows and columns, and the values themselves.
-    def __init__(self, nRows, nCols, values):
-
-        self.nRows = nRows
-        self.nCols = nCols
-
+"""
+Here are some fun notes about sectors:
+    Given a row number r, and column number c:
+    (r/3)*3 + c/3
+        tells you which of the 9 sectors your value is in.
+    c - (c/3)*3 + (r%3)*3
+        tells you which of the 9 slots in a sector your value is in.
+"""
+            
+class Sudoku(Matrix):
+    def __init__(self, values):
         # Checks if there are enough values.
-        if nRows * nCols != len(values):
-            raise TypeError("A matrix of "+str(nRows)+" Rows and "+str(nCols)+" Columns"+\
-            " requires precisely "+str(nRows*nCols)+" values.")
-        
-        # The actual values are stored in a dictionary, with coordinates such as (0,0) & (1,2)
-        # being the keys.
+        if 81 != len(values):
+            raise TypeError("Sudoku puzzles require precisely 81 values.")
+        # Checks all values are integers 1-9.
+        for value in values:
+            if   not type(value) == int:
+                raise TypeError("Sudoku puzzles require integers.")
+            elif not 0 < value < 10:
+                print value
+                raise TypeError("Sudoku puzzles require integers 1-9.")
+
         self.M = {}
-        
-        # What sets apart a Matrix from a Dictionary is being able to access the rows/columns.
         self.row = []
         self.col = []
+        self.sec = []               # Sec refers to a Section, a 3x3 box in a Sudoku grid.
         
-        # The index is only used to keep track of which value needs to be recorded.
+        # The row/col/sec lists are given 9 sublists ahead of time.
+        for w in xrange(9):
+            self.row.append([])
+            self.col.append([])
+            self.sec.append([])
+            
         index = -1
-        
-        for x in xrange(nRows):
-            for y in xrange(nCols):
+        for r in xrange(9):
+            for c in xrange(9):
                 index += 1
-                
-                # For each value, the key is a string
-                Key = str(x)+","+str(y)
+                Key = str(r)+","+str(c)
                 self.M[Key] = values[index]
-
-                
-                # If the row does not yet exist, create the list
-                try:
-                    self.row[x].append(values[index])
-                except IndexError:
-                    self.row.append([])
-                    self.row[x].append(values[index])
-                # If the col does not yet exist, create the list
-                try:
-                    self.col[y].append(values[index])
-                except IndexError:
-                    self.col.append([])
-                    self.col[y].append(values[index])
+                self.row[r].append(values[index])
+                self.col[c].append(values[index])
+                # Adds values to their appropriate sector.
+                self.sec[((r/3)*3)+(c/3)].append(values[index])
 
     def __str__(self):
+        """The only difference between this string and the original string is that sectors are
+        more clearly visible, and brackets are used instead."""
         string = ""
-        for row in self.row:
-            string += "(  "
-            for value in row:
-                string += str(value)+"  "
-            string += ")\n"
+        for row in xrange(9):
+            string += "[ "
+            for value in xrange(9):
+                string += str(self.row[row][value])+" "
+
+                # Inserting vertical seperators for sectors
+                if value == 2 or value == 5:
+                    string += "| "
+            string += "]\n"
+            
+            # Inserting horizontal seperators for sectors
+            if row == 2 or row == 5:
+                string += ("_"*25)+"\n"
         return string
+
+    def isvalid(self):
+        """Returns True if the puzzle is a valid Sudoku puzzle, False if not."""
+        
+        def any_duplicates(theList):
+            """Checks if there are any duplicates in a list."""
+            for A in xrange(len(theList)):
+                for B in xrange(A+1,len(theList)):
+                    if theList[A] == theList[B]:
+                        return True
+            return False
+
+        for row in self.row:
+            if any_duplicates(row):     return False
+        for col in self.col:
+            if any_duplicates(col):     return False
+        for sec in self.sec:
+            if any_duplicates(sec):     return False    
+        return True
 
     def setvalue(self,r,c,value):
         self.M[str(r)+","+str(c)] = value
         self.row[r][c] = value
         self.col[c][r] = value
+        self.sec[(r/3)*3 + c/3][c - (c/3)*3 + (r%3)*3] = value
 
     def setrow(self,r,values):
         # Checks this request will work in the first place
@@ -122,106 +149,8 @@ class Matrix(object):
             row[c2] = temp
 
 
-### HEY.
-### Here are some fun notes about sectors.
-### Given a row number r, and column number c:
-### (r/3)*3 + c/3
-###  tells you which of the 9 sectors your value is in.
-### c - (c/3)*3 + (r%3)*3
-###  tells you which of the 9 slots in a sector your value is in.
-### ...
-### To tell you the truth, an 81-value dictionary is probably much faster than solving 81
-### math problems. But I'm a little too proud of myself for figuring this out anyway, so...
-### THANKS FOR READING!!
-            
-class Sudoku(Matrix):
 
-    def __init__(self, values):
-        # Checks if there are enough values.
-        if 81 != len(values):
-            raise TypeError("Sudoku puzzles require precisely 81 values.")
-        # Checks all values are integers 1-9.
-        for value in values:
-            if   not type(value) == int:
-                raise TypeError("Sudoku puzzles require integers.")
-            elif not 0 < value < 10:
-                print value
-                raise TypeError("Sudoku puzzles require integers 1-9.")
-
-        self.M = {}
-        self.row = []
-        self.col = []
-        self.sec = []               # Sec refers to a Section, a 3x3 box in a Sudoku grid.
-        
-        # The row/col/sec lists are given 9 sublists ahead of time.
-        for w in xrange(9):
-            self.row.append([])
-            self.col.append([])
-            self.sec.append([])
-            
-        index = -1
-        for r in xrange(9):
-            for c in xrange(9):
-                index += 1
-                Key = str(r)+","+str(c)
-                self.M[Key] = values[index]
-                self.row[r].append(values[index])
-                self.col[c].append(values[index])
-                # Adds values to their appropriate sector.
-                self.sec[((r/3)*3)+(c/3)].append(values[index])
-
-
-    
-    def __str__(self):
-        """The only difference between this string and the original string is that sectors are
-        more clearly visible, and brackets are used instead."""
-        string = ""
-        for row in xrange(9):
-            string += "[ "
-            for value in xrange(9):
-                string += str(self.row[row][value])+" "
-
-                # Inserting vertical seperators for sectors
-                if value == 2 or value == 5:
-                    string += "| "
-            string += "]\n"
-            
-            # Inserting horizontal seperators for sectors
-            if row == 2 or row == 5:
-                string += ("_"*25)+"\n"
-        return string
-
-
-    
-    def isvalid(self):
-        """Returns True if the puzzle is a valid Sudoku puzzle, False if not."""
-        
-        def any_duplicates(theList):
-            """Checks if there are any duplicates in a list."""
-            for A in xrange(len(theList)):
-                for B in xrange(A+1,len(theList)):
-                    if theList[A] == theList[B]:
-                        return True
-            return False
-
-        for row in self.row:
-            if any_duplicates(row):     return False
-        for col in self.col:
-            if any_duplicates(col):     return False
-        for sec in self.sec:
-            if any_duplicates(sec):     return False    
-        return True
-
-
-
-    def setvalue(self,r,c,value):
-        self.M[str(r)+","+str(c)] = value
-        self.row[r][c] = value
-        self.col[c][r] = value
-        self.sec[(r/3)*3 + c/3][c - (c/3)*3 + (r%3)*3] = value
-
-# Some serious restructuring is in order.
-        
+# This just makes my life easier for testing        
 solved_puzzle = [2,4,8,3,9,5,7,1,6,5,7,1,6,2,8,3,4,9,9,3,6,7,4,1,5,8,2,6,8,2,5,3,9,1,7,4,3,5, \
                  9,1,7,4,6,2,8,7,1,4,8,6,2,9,5,3,8,6,3,4,1,7,2,9,5,1,9,5,2,8,6,4,3,7,4,2,7,9, \
                  5,3,8,6,1]
